a35509e7c58b525020c7f30a22d56c3cb3aacfe4
biruk1122 <birukfikadu29@gmail.com>
2026-02-18 17:18:14 +0300

    generating the AllProducts page

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -16,6 +16,7 @@ import toast from "react-hot-toast";
    const navigate = useNavigate();
 
    const [cartItems, setCartItems] = useState({});
+   const [searchQuery, setSearchQuery] = useState({});
 
    const fetchProducts = async () => {
     setProducts(dummyProducts);
@@ -59,7 +60,7 @@ import toast from "react-hot-toast";
     fetchProducts();
    }, []);
 
-   const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems};
+   const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery};
 
    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 
